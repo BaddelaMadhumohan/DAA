ae2ca56e98550614db92a7d49f0a80c512d0ec3e
BaddelaMadhumohan <baddelamadhumohan2006@gmail.com>
2026-07-22 12:55:50 +0530

    Update Practical2Program.cpp

--- Practical2Program.cpp
+++ Practical2Program.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <algorithm>
 using namespace std;
+using namespace chrono;
 
 // Linear Search Function
 int linearSearch(int arr[], int n, int key)
 {
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-        if (arr[i] == key)
+        if(arr[i] == key)
         {
             return i;
         }
@@ -20,15 +24,15 @@ int binarySearch(int arr[], int n, int key)
     int low = 0;
     int high = n - 1;
 
-    while (low <= high)
+    while(low <= high)
     {
         int mid = (low + high) / 2;
 
-        if (arr[mid] == key)
+        if(arr[mid] == key)
         {
             return mid;
         }
-        else if (arr[mid] < key)
+        else if(arr[mid] < key)
         {
             low = mid + 1;
         }
@@ -43,37 +47,51 @@ int binarySearch(int arr[], int n, int key)
 
 int main()
 {
-    int n, key;
-
-    cout << "Enter the number of elements: ";
-    cin >> n;
+    const int n = 100;
 
     int arr[n];
 
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+    srand(time(0));
 
-    cout << "Enter the element to search: ";
-    cin >> key;
+    for(int i = 0; i < n; i++)
+        arr[i] = rand() % 1000;
+
+    // Binary Search requires sorted array
+    sort(arr, arr + n);
+
+    // Random key to search
+    int key = arr[rand() % n];
+
+    cout << "Number of Elements = " << n << endl;
+    cout << "Search Key = " << key << "\n\n";
 
     // Linear Search
+    auto start = high_resolution_clock::now();
     int linearResult = linearSearch(arr, n, key);
+    auto stop = high_resolution_clock::now();
 
-    if (linearResult != -1)
-        cout << "\nLinear Search: Element found at index " << linearResult << endl;
+    cout << "Linear Search Time : "
+         << duration_cast<microseconds>(stop - start).count()
+         << " microseconds\n";
+
+    if(linearResult != -1)
+        cout << "Element Found at Index : " << linearResult << "\n\n";
     else
-        cout << "\nLinear Search: Element not found" << endl;
+        cout << "Element Not Found\n\n";
 
     // Binary Search
+    start = high_resolution_clock::now();
     int binaryResult = binarySearch(arr, n, key);
+    stop = high_resolution_clock::now();
 
-    if (binaryResult != -1)
-        cout << "Binary Search: Element found at index " << binaryResult << endl;
+    cout << "Binary Search Time : "
+         << duration_cast<microseconds>(stop - start).count()
+         << " microseconds\n";
+
+    if(binaryResult != -1)
+        cout << "Element Found at Index : " << binaryResult << endl;
     else
-        cout << "Binary Search: Element not found" << endl;
+        cout << "Element Not Found" << endl;
 
     return 0;
 }
